539aff621ba5352d16769541c217a8fffb086194
Pxddy <patrick.henke@t-online.de>
2023-01-15 20:05:20 +0100

    not needed

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -7,8 +6,4 @@ plugins {
     alias(libs.plugins.hilt.gradlePlugin) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
