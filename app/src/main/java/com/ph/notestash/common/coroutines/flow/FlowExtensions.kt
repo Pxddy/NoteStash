4f980575a768ea8d216e5e289f28deea75147956
Pxddy <patrick.henke@t-online.de>
2023-01-15 17:16:30 +0100

    Adjusted flow extensions

--- app/src/main/java/com/ph/notestash/common/coroutines/flow/FlowExtensions.kt
+++ app/src/main/java/com/ph/notestash/common/coroutines/flow/FlowExtensions.kt
@@ -5,21 +5,20 @@ import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
 /**
- * Converts the [Flow] into a [StateFlow] with null as the initial value. [filterNotNull] is used to
- * return a flow which does not contain the null emission.
+ * Converts a [Flow] into a [SharedFlow]. It replays the last emission to new subscribers.
  */
-fun <T : Any> Flow<T>.shareLatest(
+fun <T> Flow<T>.shareLatest(
     scope: CoroutineScope,
     started: SharingStarted = SharingStarted.WhileSubscribed(replayExpirationMillis = 0)
-): Flow<T> = stateIn(scope = scope, started = started, initialValue = null)
-    .filterNotNull()
+): SharedFlow<T> = conflate()
+    .shareIn(scope = scope, started = started, replay = 1)
 
 /**
  * This is a shorthand for `scope.launch { flow.collectLatest(action) }`
  */
-fun <T> Flow<T>.collectLatestIn(
+inline fun <T> Flow<T>.collectLatestIn(
     scope: CoroutineScope,
-    action: suspend (value: T) -> Unit
+    crossinline action: suspend (value: T) -> Unit
 ) = scope.launch {
-    collectLatest(action)
+    collectLatest { action(it) }
 }
