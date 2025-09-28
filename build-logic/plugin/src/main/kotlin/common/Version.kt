eee06b7769e5a11b896edc103184fe8b875d4b02
Pxddy <patrick.henke@t-online.de>
2025-09-28 20:45:01 +0200

    bump android sdk to 36

--- build-logic/plugin/src/main/kotlin/common/Version.kt
+++ build-logic/plugin/src/main/kotlin/common/Version.kt
@@ -17,8 +17,8 @@ object Version {
 
     object Sdk {
         const val min = 23
-        const val target = 35
-        const val compile = 35
+        const val target = 36
+        const val compile = 36
     }
 
     object Java {
